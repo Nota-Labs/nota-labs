146580f46e1498fc502e6c9970ab463d8d804802
Ben Dunn <bendunn135@gmail.com>
2019-11-04 20:52:01 +0000

    Move app to root

---
+++ build/precache-manifest.0ab0d921f902ea45fb6c7062385fe3c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "311a10239d123b2a3a1cca32e9230a64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d20cdff6cc065f9f8db9",
+    "url": "/scripts/bundle.c2d4b39.js"
+  },
+  {
+    "revision": "84e61b3300ba4d6ed44035dc35654877",
+    "url": "/scripts/bundle.c2d4b39.js.LICENSE"
+  },
+  {
+    "revision": "1de85fd80abc66cb16f9",
+    "url": "/scripts/modernizr.c2d4b39.js"
+  },
+  {
+    "revision": "98fe731926ca7c3ac473a550f124342a",
+    "url": "/scripts/modernizr.c2d4b39.js.LICENSE"
+  },
+  {
+    "revision": "3e0216a8a635c2aed765701e35802673",
+    "url": "/static/icon.c2d4b39.svg"
+  },
+  {
+    "revision": "8d362760b25279384e2466f9bd832ce8",
+    "url": "/static/react-redux-saga.c2d4b39.svg"
+  }
+]);
