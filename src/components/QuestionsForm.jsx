c3fa45224cbfe305124279943fab37956e8b80b5
Ben Dunn <bendunn135@gmail.com>
2019-11-04 22:21:21 +0000

    Radio button styles

--- src/components/QuestionsForm.jsx
+++ src/components/QuestionsForm.jsx
@@ -2,30 +2,78 @@ import React from 'react';
 import { reduxForm, Field } from 'redux-form';
 import Button from 'react-bootstrap/Button';
 import FormCheck from 'react-bootstrap/FormCheck';
-import FormControl from 'react-bootstrap/FormControl';
+// import FormControl from 'react-bootstrap/FormControl';
+import styled from 'styled-components';
+
+const RadioInputCustom = styled.div`
+  padding-left: 2em;
+  margin-bottom: 1em;
+  label {
+    &:hover {
+      cursor: pointer;
+      background: #f5f5f5;
+    }
+    &::before{
+      content: '';
+      display: block;
+      width: 1.4em;
+      height: 1.4em;
+      position: absolute;
+      top: 50%;
+      margin-top: -0.7em;
+      margin-left: -3.15em;
+      border-radius: 1.4em;
+      background-color: transparent;
+      border: 2px solid #53dbb5;
+    }
+    background: white;
+    border-radius: 5px;
+    border: 1px solid transparent;
+    margin-left: -3em;
+    padding: 1.5em 2em 1.5em 5em;
+      p {
+        font-weight: 300;
+        font-size: 1em;
+        margin-bottom: 0;
+      }
+      strong {
+        font-weight: 400;
+        font-size: 1.5em;
+      }
+    }
+  }
+  input[type="radio"]:checked+label {
+    border-color: #ccc;
+    &::before{
+      background-color: #53dbb5;
+   }
+  }
+`;
 
 const RadioField = ({ input, meta, label, step, id, ...props }) => (
-  <FormCheck
-    inline
-    id={`input${id}`}
-    name={`step${step}`}
-    label={label}
-    type="radio"
-    {...input}
-    {...props}
-  />
+  <RadioInputCustom>
+    <FormCheck
+      inline
+      id={`input${id}`}
+      name={`step${step}`}
+      label={label}
+      type="radio"
+      {...input}
+      {...props}
+    />
+  </RadioInputCustom>
 );
 
-const FieldInput = ({ input, meta, id, type, placeholder, label, ...props }) => (
-  <FormControl
-    id={`input${id}`}
-    type={type}
-    placeholder={placeholder}
-    label={label}
-    {...input}
-    {...props}
-  />
-);
+// const FieldInput = ({ input, meta, id, type, placeholder, label, ...props }) => (
+//   <FormControl
+//     id={`input${id}`}
+//     type={type}
+//     placeholder={placeholder}
+//     label={label}
+//     {...input}
+//     {...props}
+//   />
+// );
 
 const QuizForm = props => {
   const { handleSubmit, submitting, currentQuestion, currentStep } = props;
@@ -41,6 +89,7 @@ const QuizForm = props => {
           component={RadioField}
           step={currentStep}
           props={{ value: index }}
+          key={`option_${index}`}
           label={
             <span>
               <strong>{answer.title}</strong>
